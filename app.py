9d6f7c52b594ef4e906086325d21f2e15bc4d0e7
Sudhamshu Hosamane <sudhamshumh@gmail.com>
2023-05-29 23:04:33 -0500

    Updated eps-greedy

--- app.py
+++ app.py
@@ -1,151 +1,105 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from bandit import EpsilonGreedy
+
+def run_simulation(bandit, true_rewards, num_iterations, arm_chart, regret_chart, prob_chart):
+	rewards = np.zeros(num_iterations)
+	cumulative_regrets = np.zeros(num_iterations)
+	optimal_arm = np.argmax(true_rewards)
+	for i in range(num_iterations):
+		arm = bandit.select_arm()
+		reward = np.random.binomial(1, true_rewards[arm])  # binary reward
+		bandit.update(arm, reward)
+		rewards[i] = reward
+		cumulative_regrets[i] = (i+1) * true_rewards[optimal_arm] - np.sum(rewards)
+		if (i+1) % 100 == 0:  # update every 100 iterations
+			arm_chart.bar_chart(bandit.arm_counts / (i+1))
+			fig = plot_regret(cumulative_regrets[:i+1], i+1)
+			regret_chart.pyplot(fig)
+			prob_chart.bar_chart(bandit.arm_rewards)
+			plt.close(fig)
+	return rewards, cumulative_regrets
+
+
+def plot_regret(cumulative_regrets, num_iterations):
+	fig, ax = plt.subplots()
+	ax.plot(cumulative_regrets, label="Cumulative Regret")
+	ax.plot(cumulative_regrets / np.arange(1, num_iterations+1), label="Average Regret")
+	ax.legend()
+	ax.set_xlabel('Iteration')
+	ax.set_ylabel('Regret')
+	return fig
 
 def main():
-    st.title("Multi-Armed Bandits Tutorial")
+	st.title("Multi-Armed Bandits Tutorial")
 
-    # Introduction to Multi-Armed Bandits
-    st.header("Introduction")
+	# Introduction to Multi-Armed Bandits
+	st.header("Introduction")
 
-    st.write("Imagine you walk into a casino and see a row of slot machines, each with a different lever or button to pull. You want to make as much money as possible, but you don't know which machines are the most likely to pay out. More formally, suppose there's a true distribution of rewards for each of the slot machines (which you are unaware of), and you need to come up with a strategy to maximize your potential winnings. This is the essence of the multi-armed bandits problem.")
+	st.write("Imagine you walk into a casino and see a row of slot machines, each with a different lever or button to pull. You want to make as much money as possible, but you don't know which machines are the most likely to pay out. More formally, suppose there's a true distribution of rewards for each of the slot machines (which you are unaware of), and you need to come up with a strategy to maximize your potential winnings. This is the essence of the multi-armed bandits problem.")
 
-    st.write("In real-world scenarios, multi-armed bandits have numerous applications where the problem is set up as previously defined. For example, in online advertising, you want to determine which ad to display to users to maximize click-through rates. Each ad can be considered an 'arm', and the goal is to learn which ad has the highest click-through rate by exploring and exploiting different options.")
+	st.write("In real-world scenarios, multi-armed bandits have numerous applications where the problem is set up as previously defined. For example, in online advertising, you want to determine which ad to display to users to maximize click-through rates. Each ad can be considered an 'arm', and the goal is to learn which ad has the highest click-through rate by exploring and exploiting different options.")
 
-    st.write("Another scenario is in clinical trials, where researchers need to identify the most effective treatment from a set of options. By applying multi-armed bandits, they can allocate patients to different treatments and adapt their strategy based on the observed outcomes. This adaptive approach allows for efficient exploration of different treatments and maximizes the chances of identifying the best one.")
+	st.write("Another scenario is in clinical trials, where researchers need to identify the most effective treatment from a set of options. By applying multi-armed bandits, they can allocate patients to different treatments and adapt their strategy based on the observed outcomes. This adaptive approach allows for efficient exploration of different treatments and maximizes the chances of identifying the best one.")
 
-    st.subheader("Exploration vs. Exploitation")
+	st.subheader("Exploration vs. Exploitation")
 
-    st.markdown("""
-    The multi-armed bandit problem is a classic dilemma in decision-making under uncertainty. It refers to a scenario where an agent (or gambler) is faced with a set of slot machines (or bandits), each with a different unknown probability distribution of rewards. The agent's goal is to maximize its cumulative reward over a series of sequential actions, while simultaneously balancing the exploration of potentially better options and the exploitation of known good options.
+	st.markdown("""
+	The multi-armed bandit problem is a classic dilemma in decision-making under uncertainty. Recall that your goal is to maximize the cumulative reward over a series of sequential actions (draws from the slot machine). You can achieve this while simultaneously balancing the exploration of potentially better options and the exploitation of known good options.
 
-    **Exploration** involves trying out different arms to gather information about their reward probabilities. It allows the agent to discover potentially better arms.
+	**Exploration** involves trying out different arms to gather information about their reward probabilities. It allows the agent to discover potentially better arms.
 
-    **Exploitation** involves choosing the arm that is estimated to have the highest reward probability based on the information collected so far. It maximizes the immediate reward.
+	**Exploitation** involves choosing the arm that is estimated to have the highest reward probability based on the information collected so far. It maximizes the immediate reward.
 
-    The multi-armed bandit problem aims to find the optimal tradeoff between exploration and exploitation to maximize the cumulative reward over time.
+	The multi-armed bandit problem aims to find the optimal tradeoff between exploration and exploitation to maximize the cumulative reward over time.
 
-    There are two main categories of algorithms used to solve the multi-armed bandit problem:
+	There are two main categories of algorithms used to solve the multi-armed bandit problem:
 
-    - **Frequentist Approaches**: These algorithms use frequentist statistics to estimate the arm probabilities and make decisions based on past observations. Two popular frequentist algorithms are the *epsilon-greedy* and *Upper Confidence Bound (UCB)* methods.
+	- **Frequentist Approaches**: These algorithms use frequentist statistics to estimate the arm probabilities and make decisions based on past observations. Two popular frequentist algorithms are the *epsilon-greedy* and *Upper Confidence Bound (UCB)* methods.
 
-    - **Bayesian Approaches**: These algorithms use Bayesian statistics to model the uncertainty of arm probabilities and update their beliefs based on new observations. One commonly used Bayesian algorithm is *Bernoulli Thompson Sampling*.
+	- **Bayesian Approaches**: These algorithms use Bayesian statistics (updating prior beliefs about the distributions) to model the uncertainty of arm probabilities and update their beliefs based on new observations. One commonly used Bayesian algorithm is *Bernoulli Thompson Sampling*.
 
-    In this tutorial, we will delve into these algorithms and explore how they tackle the exploration-exploitation tradeoff to solve the multi-armed bandit problem.
-    """)
+	In this tutorial, we will delve into these algorithms and explore how they tackle the exploration-exploitation tradeoff to solve the multi-armed bandit problem.
+	""")
 
-    # Epsilon-Greedy Algorithm
-    st.header("Epsilon-Greedy Algorithm")
-    st.write("The Epsilon-Greedy algorithm is a classic and straightforward approach to balancing exploration and exploitation.")
+	# Epsilon-Greedy Algorithm
+	st.header("Epsilon-Greedy Algorithm")
+	st.write("The Epsilon-Greedy algorithm is a classic and straightforward approach to balancing exploration and exploitation.")
 
-    st.write("The algorithm maintains estimates of the expected rewards for each arm. It primarily exploits the arm with the highest estimated reward (greedy action) but also explores other arms with a small probability epsilon.")
+	st.write("The algorithm maintains estimates of the expected rewards for each arm. It primarily exploits the arm with the highest estimated reward (greedy action) but also explores other arms with a small probability epsilon.")
 
-    st.write("Mathematically, let's define:")
-    st.latex(r"Q(a) = \text{{Estimated reward of arm }} a")
-    st.latex(r"N(a) = \text{{Number of times arm }} a \text{{ has been selected}}")
+	st.write("Mathematically, let's define:")
+	st.latex(r"Q(a) = \text{{Estimated reward of arm }} a")
+	st.latex(
+		r"N(a) = \text{{Number of times arm }} a \text{{ has been selected}}")
 
-    st.write("The algorithm works as follows:")
-    st.write("- Initialize Q(a) and N(a) for each arm a")
-    st.write("- With probability epsilon, select a random arm (exploration)")
-    st.write("- Otherwise, select the arm with the highest estimated reward (exploitation)")
-    st.write("- Update the estimated reward and selection count for the chosen arm based on the observed reward")
+	st.write("The algorithm works as follows:")
+	st.write("- Initialize Q(a) and N(a) for each arm a")
+	st.write("- With probability epsilon, select a random arm (exploration)")
+	st.write(
+		"- Otherwise, select the arm with the highest estimated reward (exploitation)")
+	st.write("- Update the estimated reward and selection count for the chosen arm based on the observed reward")
 
-    st.write("The Epsilon-Greedy algorithm is simple to implement and effective in scenarios with relatively stable reward distributions.")
+	st.write("The Epsilon-Greedy algorithm is simple to implement and effective in scenarios with relatively stable reward distributions.")
 
-    # Upper Confidence Bound (UCB) Algorithm
-    st.header("Upper Confidence Bound (UCB) Algorithm")
-    st.write("The Upper Confidence Bound (UCB) algorithm uses the principle of optimism in the face of uncertainty to balance exploration and exploitation.")
+	num_arms = st.selectbox("Choose the number of arms", list(range(2, 6)))
+	true_rewards = [st.slider(f"Set the true reward for arm {i+1}", 0.0, 1.0) for i in range(num_arms)]
+	num_iterations = st.slider("Choose the number of iterations", 100, 10000)
+	epsilon = st.slider("Choose the value of epsilon", 0.0, 1.0)
+	epsilon_choice = st.selectbox("Choose the type of epsilon", ['Fixed', 'Adaptive'])
 
-    st.write("The algorithm maintains upper confidence bounds for the expected rewards of each arm. It selects the arm with the highest upper confidence bound, which takes into account both the estimated reward and the uncertainty.")
+	arm_chart = st.empty()  # placeholder for arm chart
+	regret_chart = st.empty()  # placeholder for regret chart
+	prob_chart = st.empty()  # placeholder for probability chart
 
-    st.write("Mathematically, let's define:")
-    st.latex(r"\overline{X}(a) = \frac{1}{N(a)} \sum_{t=1}^{N(a)} X_{t}(a)")
-    st.latex(r"UCB(a) = \overline{X}(a) + \sqrt{\frac{2 \log(N)}{N(a)}}")
+	adaptive = (epsilon_choice == 'Adaptive')
+	bandit = EpsilonGreedy(num_arms, epsilon, adaptive)
+	rewards, cumulative_regrets = run_simulation(bandit, true_rewards, num_iterations, arm_chart, regret_chart, prob_chart)
 
-    st.write("The algorithm works as follows:")
-    st.write("- Initialize N(a) and select each arm once")
-    st.write("- For each subsequent selection:")
-    st.write("    - Calculate the upper confidence bound (UCB) for each arm")
-    st.write("    - Select the arm with the highest UCB (exploitation)")
-    st.write("    - Update the selection count for the chosen arm")
-    st.write("    - Update the estimated reward for the chosen arm based on the observed reward")
 
-    st.write("The UCB algorithm adapts to the observed rewards and is particularly useful when dealing with uncertain or non-stationary reward distributions.")
-
-    # Bernoulli Thompson Sampling Algorithm
-    st.header("Bernoulli Thompson Sampling Algorithm")
-    st.write("The Bernoulli Thompson Sampling algorithm, also known as Bayesian Bandit, leverages Bayesian inference to estimate arm probabilities.")
-
-    st.write("The algorithm starts with prior distributions over the arm probabilities and updates them based on the observed rewards. In each iteration, it samples from the posterior distributions and selects the arm with the highest sample.")
-
-    st.write("Mathematically, let's define:")
-    st.latex(r"\text{{Beta}}(\alpha, \beta) = \text{{Beta distribution with parameters }} \alpha \text{{ and }} \beta")
-    st.latex(r"\text{{Beta}}(\alpha + R(a), \beta + N(a) - R(a)) = \text{{Posterior distribution of arm }} a")
-
-    st.write("The algorithm works as follows:")
-    st.write("- Initialize the prior distributions for each arm")
-    st.write("- For each selection:")
-    st.write("    - Sample from the posterior distribution of each arm")
-    st.write("    - Select the arm with the highest sample (exploitation)")
-    st.write("    - Update the observed reward and selection count for the chosen arm")
-
-    st.write("Bernoulli Thompson Sampling adapts to sparse and non-stationary reward distributions, making informed decisions based on posterior beliefs.")
-
-    # Evaluation Metrics: Average Cumulative Regret and Simple Regret
-    st.header("Evaluation Metrics for Multi-Armed Bandit algorithms: Average Cumulative Regret and Simple Regret")
-
-    st.write("When evaluating multi-armed bandit algorithms, we need metrics to measure their performance. Two commonly used metrics are Average Cumulative Regret and Simple Regret.")
-
-    st.subheader("Simple Regret")
-
-    st.write("Simple Regret, also known as Instantaneous Regret, measures the difference between the expected reward of the optimal arm and the reward obtained from the chosen arm at each time step t.")
-
-    st.write("Mathematically, Simple Regret at time t can be defined as:")
-    st.latex(r"S(t) = \max_{a^\ast \in \mathcal{A}} \mu^\ast - \mu_{A(t)}")
-
-    st.write("where:")
-    st.latex(r"S(t) = \text{Simple Regret at time t}")
-    st.latex(r"a^\ast = \text{Optimal arm with the highest reward probability}")
-    st.latex(r"\mu^\ast = \text{Expected reward of the optimal arm}")
-    st.latex(r"\mu_{A(t)} = \text{Expected reward of the chosen arm at time t}")
-
-    st.write("Simple Regret provides a measure of how much reward is lost at each time step by not selecting the optimal arm.")
-
-    st.subheader("Average Cumulative Regret")
-
-    st.write("While Simple Regret captures the regret at each time step, Average Cumulative Regret sums up the regrets over a sequence of trials and provides a more comprehensive measure of the algorithm's performance.")
-
-    st.write("Mathematically, Average Cumulative Regret after T trials can be defined as:")
-    st.latex(r"R(T) = T \mu^\ast - \sum_{t=1}^{T} \mu_{A(t)}")
-
-    st.write("where:")
-    st.latex(r"R(T) = \text{Average Cumulative Regret after T trials}")
-    st.latex(r"T = \text{Number of trials}")
-    st.latex(r"\mu^\ast = \text{Expected reward of the optimal arm}")
-    st.latex(r"\mu_{A(t)} = \text{Expected reward of the chosen arm at time t}")
-
-    st.write("Average Cumulative Regret measures the total regret accumulated over T trials compared to always selecting the optimal arm. A lower Average Cumulative Regret indicates better performance.")
-
-    st.write("By evaluating multi-armed bandit algorithms using these metrics, we can compare their effectiveness in maximizing rewards and minimizing regret.")
-
-    # Adaptive Experiments and Policy Interventions
-    st.header("Adaptive Experiments and Policy Interventions")
-    st.write("Adaptive experiments refer to experiments where the allocation of treatments or interventions adapts based on the observed outcomes. The goal is to identify the best policy or treatment from a set of options.")
-
-    st.write("In adaptive experiments, policies are often represented as probability distributions over the arms. Each policy specifies the probability of selecting each arm at any given time.")
-
-    st.write("Mathematically, let's define:")
-    st.latex(r"\pi = (\pi_1, \pi_2, ..., \pi_k) = \text{{Probability distribution over the arms}}")
-    st.latex(r"T(a) = \text{{Number of times arm }} a \text{{ has been selected}}")
-    st.latex(r"R(a) = \text{{Sum of rewards obtained from arm }} a")
-    st.latex(r"\hat{r}(a) = \frac{R(a)}{T(a)} = \text{{Estimated reward of arm }} a")
-
-    st.write("The objective is to maximize the cumulative reward or minimize the cumulative regret over a series of trials.")
-
-    st.write("One commonly used approach is to select the policy with the highest upper confidence bound (UCB) estimate. The UCB estimate for each policy is calculated as:")
-    st.latex(r"UCB(a) = \hat{r}(a) + \sqrt{\frac{2 \log(n)}{T(a)}}")
-
-    st.write("By choosing the policy with the highest UCB, we balance exploration (choosing arms with high uncertainty) and exploitation (choosing arms with high estimated rewards). This adaptive approach allows us to learn and adapt our policy based on the observed outcomes.")
-
-    st.write("Adaptive experiments have been successfully applied in various policy interventions. For example, in a public health campaign, policymakers can allocate resources to different interventions and adapt their strategy based on the observed impact on desired outcomes. By dynamically allocating resources based on observed data, policymakers can make informed decisions and optimize the effectiveness of their interventions.")
+	st.write("Final rewards:")
+	st.bar_chart(bandit.arm_rewards)
 
 if __name__ == "__main__":
 	main()
--- bandit.py
+++ bandit.py
@@ -1,121 +1,34 @@
-import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 
-def bernoulli_thompson_sampling(probabilities, num_iterations):
-    num_arms = len(probabilities)
-    successes = np.ones(num_arms)
-    failures = np.ones(num_arms)
-    selected_arms = []
-    cumulative_rewards = []
+class EpsilonGreedy:
+    def __init__(self, num_arms, epsilon, adaptive=False):
+        self.num_arms = num_arms
+        self.epsilon = epsilon
+        self.adaptive = adaptive
+        self.arm_rewards = np.zeros(num_arms)
+        self.arm_counts = np.zeros(num_arms)
+        self.total_count = 0
 
-    for _ in range(num_iterations):
-        sampled_theta = np.random.beta(successes, failures)
-        selected_arm = np.argmax(sampled_theta)
-        reward = simulate_rewards(probabilities[selected_arm])
-        successes[selected_arm] += reward
-        failures[selected_arm] += 1 - reward
+    def select_arm(self):
+        epsilon = self.epsilon
+        if self.adaptive:
+            if self.total_count > 0:
+                epsilon = 1.0 / (self.total_count ** (1/2))
 
-        selected_arms.append(selected_arm + 1)
-        cumulative_rewards.append(sum(successes) / sum(successes + failures))
+        if np.random.rand() < epsilon:
+            return np.random.choice(self.num_arms) # Exploration
+        else:
+            return np.argmax(self.arm_rewards) # Exploitation
 
-    return selected_arms, cumulative_rewards
+    def update(self, chosen_arm, reward):
+        self.total_count += 1
+        self.arm_counts[chosen_arm] += 1
+        count = self.arm_counts[chosen_arm]
+        value = self.arm_rewards[chosen_arm]
+        new_value = ((count - 1) / float(count)) * value + (1 / float(count)) * reward
+        self.arm_rewards[chosen_arm] = new_value
 
-def simulate_rewards(probability):
-    return np.random.binomial(1, probability)
-
-def main():
-    # Initialize Streamlit application
-    st.title("Bernoulli Thompson Sampling")
-
-    # Sidebar inputs
-    num_arms = st.sidebar.slider("Number of Arms", min_value=2, max_value=10, value=3)
-    num_iterations = st.sidebar.slider("Number of Iterations", min_value=100, max_value=10000, step=100, value=1000)
-    data_size = st.sidebar.slider("Data Size", min_value=10, max_value=1000, step=10, value=100)
-
-    # Generate arm probabilities
-    probabilities = np.random.uniform(low=0.1, high=0.9, size=num_arms)
-
-    # Run Bernoulli Thompson Sampling
-    selected_arms, cumulative_rewards = bernoulli_thompson_sampling(num_arms, num_iterations, probabilities)
-
-    # Calculate regret
-    optimal_arm = np.argmax(probabilities)
-    regret = np.cumsum(probabilities[optimal_arm] - np.array([probabilities[selected_arm] for selected_arm in selected_arms]))
-
-    # Display results
-    st.header("Results")
-
-    # Show the selected arms in a plot
-    st.subheader("Selected Arms")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(selected_arms)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Selected Arm")
-    st.pyplot(fig)
-
-    # Show the cumulative rewards in a plot
-    st.subheader("Cumulative Rewards")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(cumulative_rewards)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Cumulative Rewards")
-    st.pyplot(fig)
-
-    # Show the regret in a plot
-    st.subheader("Regret")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(regret)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Regret")
-    st.pyplot(fig)
-
-    # Data generation for large data size
-    large_data_probabilities = np.random.uniform(low=0.1, high=0.9, size=num_arms)
-    large_data_selected_arms, large_data_cumulative_rewards = bernoulli_thompson_sampling(num_arms, data_size, large_data_probabilities)
-
-    # Show the selected arms for large data size in a plot
-    st.subheader(f"Selected Arms for Data Size {data_size}")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(large_data_selected_arms)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Selected Arm")
-    st.pyplot(fig)
-
-    # Show the cumulative rewards for large data size in a plot
-    st.subheader(f"Cumulative Rewards for Data Size {data_size}")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(large_data_cumulative_rewards)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Cumulative Rewards")
-    st.pyplot(fig)
-
-    # Theoretical limits and performance discussion
-    st.header("Theoretical Limits and Performance")
-
-    # Explain theoretical limits and practical performance considerations
-    st.markdown("""
-		The Bernoulli Thompson Sampling algorithm provides an effective approach to tackle the multi-armed bandit problem. However, it is important to understand its theoretical limits and practical considerations:
-
-		- **Exploration vs. Exploitation:** Bernoulli Thompson Sampling balances exploration and exploitation by using Bayesian inference. It explores different arms initially but gradually focuses on the most rewarding arms based on observations.
-
-		- **Optimal vs. Suboptimal Actions:** Bernoulli Thompson Sampling aims to identify the optimal action with the highest probability of success. However, it is possible to get stuck in suboptimal actions, especially if they were initially chosen due to chance.
-
-		- **Data Size and Convergence:** The algorithm's performance improves with more iterations, allowing it to converge towards the optimal solution. However, the convergence speed depends on the data size and arm probabilities.
-
-		- **Large Data Size:** Increasing the data size enhances the algorithm's performance and reduces the impact of initial randomness. In the plots above, you can compare the selected arms and cumulative rewards between the original data size and a larger data size.
-
-		- **Inaccurate Prior Information:** Bernoulli Thompson Sampling assumes prior knowledge of arm probabilities. If the prior information is inaccurate, it may take longer to identify the optimal action.
-
-		- **Contextual Bandits:** Bernoulli Thompson Sampling is a non-contextual bandit algorithm, which means it does not consider contextual information such as demographics. If your application involves contextual information, you may consider using contextual bandit algorithms instead.
-
-		- **Real-World Applications:** Bernoulli Thompson Sampling has been successfully applied to various domains, including online advertising, recommendation systems, and clinical trials.
-
-		By experimenting with different parameters and data sizes, you can gain insights into how Bernoulli Thompson Sampling performs under different scenarios and explore its theoretical limits.
-
-		Feel free to adjust the sliders in the sidebar to explore different settings and observe the algorithm's behavior.
-		""")
-
-
-if __name__ == "__main__":
-    main()
+    def reset(self):
+        self.total_count = 0
+        self.arm_rewards = np.zeros(self.num_arms)
+        self.arm_counts = np.zeros(self.num_arms)
